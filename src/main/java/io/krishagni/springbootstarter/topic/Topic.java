275f6b85a367d87a94dd148c0c70112d562938e4
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2020-12-02 18:55:09 +0530

    Topic Repository added

--- src/main/java/io/krishagni/springbootstarter/topic/Topic.java
+++ src/main/java/io/krishagni/springbootstarter/topic/Topic.java
@@ -1,8 +1,12 @@
 package io.krishagni.springbootstarter.topic;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Topic {
 
-	
+	@Id
 	private String id;
 	private String name;
 	private String description;
